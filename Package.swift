3fb426b208fd084a436bd8599fcae2a59df5e96c
James Mark <james.david.mark@gmail.com>
2025-12-09 19:14:11 -0600

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftMusicBrainz",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SwiftMusicBrainz",
+            targets: ["SwiftMusicBrainz"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftMusicBrainz"
+        ),
+        .testTarget(
+            name: "SwiftMusicBrainzTests",
+            dependencies: ["SwiftMusicBrainz"]
+        ),
+    ]
+)
