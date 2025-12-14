095d158d14da60fb9000626f30a0bbe030c630de
James Mark <james.david.mark@gmail.com>
2025-12-14 17:45:08 -0600

    Removing unecessary authtoken

--- Sources/SwiftMusicBrainz/MusicBrainzConfig.swift
+++ Sources/SwiftMusicBrainz/MusicBrainzConfig.swift
@@ -8,18 +8,15 @@ import Foundation
 
 public struct MusicBrainzConfig {
     static let baseUrl = URL(string: "https://musicbrainz.org/ws/2")!
-    public var authToken: String
     public var appName: String
     public var appVersion: String
     public var contactInfo: String
 
     public init(
-        authToken: String,
         appName: String,
         appVersion: String,
         contactInfo: String
     ) {
-        self.authToken = authToken
         self.appName = appName
         self.appVersion = appVersion
         self.contactInfo = contactInfo
